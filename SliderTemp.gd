3fc9d7d213f547d22c0456d281f68ff67fd724b2
enrip99 <enric.1999@hotmail.com>
2024-05-02 16:39:42 +0200

    petits errors

--- SliderTemp.gd
+++ SliderTemp.gd
@@ -3,5 +3,5 @@ extends HSlider
 onready var valor = $"../ValueTemp"
 
 func _process(_delta):
-	var tempstring = "{value}%"
+	var tempstring = "{value}º"
 	valor.set_text(tempstring.format({"value":value}))
